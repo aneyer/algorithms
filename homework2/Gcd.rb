e2f21b6cff2a6bc6e9049da9211ee903407ab079
Allison Neyer <waterrose9@aol.com>
2015-03-02 16:25:15 -0800

    hw 2

---
+++ homework2/Gcd.rb
@@ -0,0 +1,20 @@
+class Gcd
+
+
+
+def gcd(x, y)
+  return y == 0 ? x : gcd(y, x % y)
+end
+
+def lcm(x,y)
+	return (x*y)/gcd(x,y)
+end
+
+# x = Gcd.new
+# puts "hi"
+# puts "gcd(2,3): "
+# puts "gcd(2,3):  #{x.gcd(2,3)}."
+# puts "lcm(12,18):  #{x.lcm(12,18)}."
+
+
+end
