7a81bbe4db1f8ac2d5349cfd28ca12d2ef15279c
Allison Neyer <waterrose9@aol.com>
2015-04-27 18:20:40 -0700

    subsetSum and spanning tree problem answers adjusted

--- homework4/originalSubsetSum.c
+++ homework4/originalSubsetSum.c
@@ -7,7 +7,7 @@
 //Given a set of non-negative integers, and a value sum, determine if there is a subset of the given set with sum equal to given sum.
 // Returns true if there is a subset of set[] with sum equal to given sum
 //assumption: function can't use one element repeatedly to make the sum. So, for example, the function can't repeatedly add one element of 3 to get 6. 
-//assumption 2: if sum is 0 it is always true because you just pick nothing (the empty set is always a subset of a set).
+//assumption 2: if sum is 0 it is always true because you just pick nothing (The empty set is always a subset of every set).
 bool isSubsetSum(int set[], int n, int sum){
     // The value of subset[i][j] will be true if there is a subset of set[0..j-1]
     //  with sum equal to i
@@ -21,7 +21,7 @@ bool isSubsetSum(int set[], int n, int sum){
     for (int i = 1; i <= sum; i++){
       subset[i][0] = false;
 	}
-     // Fill the subset table in botton up manner
+     // Fill the subset table in bottom up manner
      for (int i = 1; i <= sum; i++){
        for (int j = 1; j <= n; j++){
 			subset[i][j] = subset[i][j-1];
